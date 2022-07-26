dd74db945329a0aa055a129a5961d3baae3ccc61
stonebo <stonebo@STONEBO.NET>
2022-07-26 21:05:15 +0800

    chore: update __all__ method

--- notion_py/__init__.py
+++ notion_py/__init__.py
@@ -2,3 +2,10 @@
 # @Filename : __init__.py
 # @Date : 2022-07-25-10-30
 # @Project: nt-integration-sdk
+from .database import Database
+from .page import Page
+
+__all__ = [
+    "Page",
+    "Database"
+]
